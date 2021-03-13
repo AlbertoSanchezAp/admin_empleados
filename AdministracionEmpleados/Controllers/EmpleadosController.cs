85641ce58294ed2febc09a1e369a60ae6fe84862
AlbertoSanchezAp <jose.nava@coppel.com>
2021-03-13 16:32:21 -0700

    se versiona proyecto C# MVC 2019, administracion_empleados
    Signed-off-by: AlbertoSanchezAp <jose.nava@coppel.com>

---
+++ AdministracionEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,30 @@
+﻿using AdministracionEmpleados.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdministracionEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        // GET: HomeController1
+public ActionResult AltaEmpleado()
+        {
+
+            return View();
+        }
+
+        // GET: HomeController1
+        [HttpPost]
+        public ActionResult AltaEmpleado(EmpleadoModel emp)
+        {
+            int di = emp.IdEmpleado;
+            string nombre = emp.NombreEmpleado;
+            
+            return View("/Home/Index");
+        }
+    }
+}
---
+++ AdministracionEmpleados/Models/EmpleadoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdministracionEmpleados.Models
+{
+    public class EmpleadoModel
+    {
+        public int IdEmpleado { get; set; }
+        public String NombreEmpleado { get; set; }
+        public int Edad { get; set; }
+        public String Sexo { get; set; }
+
+        public int TipoEmpleado { get; set; }
+
+        public EmpleadoModel() { }
+
+        public EmpleadoModel(int idEmpleado, String nombreEmpleado, int edad, String sexo ,int tipoEmpleado) {
+            this.IdEmpleado = idEmpleado;
+            this.NombreEmpleado = nombreEmpleado;
+            this.Edad = edad;
+            this.Sexo = sexo;
+            this.TipoEmpleado = tipoEmpleado;
+        }
+
+
+
+    }
+}
